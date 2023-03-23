274729d88f18e8eeb8bc69bbf3979ca5df0a7bee
Isabel <isabqm121@gmail.com>
2023-03-23 09:25:19 -0400

    sumar 4 numeros separados por coma

--- src/calculadoraCadenas.js
+++ src/calculadoraCadenas.js
@@ -2,13 +2,17 @@ function calcularCadena(texto){
     if(texto==="")
     {
         return 0;
-    }
+    }/*
     if(texto.length==1)
     {
         return parseInt(texto);
-    }
+    }*/
     let numeros=texto.split(",");
-    let sumador=parseInt(numeros[0])+parseInt(numeros[1]);
+    let sumador=0;
+    for(var i=0;i<numeros.length;i++)
+    {
+        sumador=sumador+parseInt(numeros[i]);
+    }
     return sumador;
 }
 export default calcularCadena;
--- src/calculadoraCadenas.spec.js
+++ src/calculadoraCadenas.spec.js
@@ -6,8 +6,11 @@ describe("Calculadora de Cadenas", () => {
     it("convertir un caracter en numero ",()=>{
         expect(calcularCadena("1")).toEqual(1);
     });
-    it("separar una cadena de dos numeros por coma",()=>
+    it("sumar una cadena de dos numeros por coma",()=>
     {
         expect(calcularCadena("1,2")).toEqual(3);
     });
+    it("sumar 4 numeros separados por coma",()=>{
+        expect(calcularCadena("1,2,3,4")).toEqual(10);
+    })
   });
