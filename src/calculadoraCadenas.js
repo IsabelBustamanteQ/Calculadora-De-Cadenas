60feab6bbb56255131a60e3d7d5003f8786d1025
Isabel <isabqm121@gmail.com>
2023-03-23 09:20:19 -0400

    Sumar dos numeros separados por coma

--- src/calculadoraCadenas.js
+++ src/calculadoraCadenas.js
@@ -3,7 +3,12 @@ function calcularCadena(texto){
     {
         return 0;
     }
-    return parseInt(texto);
-    
+    if(texto.length==1)
+    {
+        return parseInt(texto);
+    }
+    let numeros=texto.split(",");
+    let sumador=parseInt(numeros[0])+parseInt(numeros[1]);
+    return sumador;
 }
 export default calcularCadena;
--- src/calculadoraCadenas.spec.js
+++ src/calculadoraCadenas.spec.js
@@ -5,5 +5,9 @@ describe("Calculadora de Cadenas", () => {
     });
     it("convertir un caracter en numero ",()=>{
         expect(calcularCadena("1")).toEqual(1);
-    })
+    });
+    it("separar una cadena de dos numeros por coma",()=>
+    {
+        expect(calcularCadena("1,2")).toEqual(3);
+    });
   });
