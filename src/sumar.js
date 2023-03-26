29c9724297371ef606c08cee6ad786f80e180cd8
Isabel <isabqm121@gmail.com>
2023-03-26 11:42:44 -0400

    Refactoring

--- src/sumar.js
+++ src/sumar.js
@@ -3,19 +3,40 @@ function sumar(cadenaDeNumeros){
     {
         return 0;
     }
-    if(cadenaDeNumeros.includes("//["))
+    let delimitadores=ObtenerDelimitadoresDefinidos(cadenaDeNumeros);
+    if(delimitadores.length>0)
     {
-        var definicionDeDelimitadores=cadenaDeNumeros.slice(cadenaDeNumeros.indexOf("[")+1,cadenaDeNumeros.lastIndexOf("]"));
-        let delimitadores=definicionDeDelimitadores.split("][");
-        var cadenaDeNumeros=cadenaDeNumeros.slice(cadenaDeNumeros.indexOf("] ")+2);
-        for(var i=0;i<delimitadores.length;i++)
-        {
-            console.log(delimitadores[i]);
-            cadenaDeNumeros=cadenaDeNumeros.replaceAll(delimitadores[i],",");
-        }
+        cadenaDeNumeros=QuitarDefinicionDeDelimitadores(cadenaDeNumeros);
+        cadenaDeNumeros=cambiarDelimitadores(cadenaDeNumeros,delimitadores);
     }
     cadenaDeNumeros=cadenaDeNumeros.replaceAll('-',',');
     let ListaDeNumeros=cadenaDeNumeros.split([","]);
+    let total_suma=sumarListaDeNumeros(ListaDeNumeros);
+    return total_suma;
+}
+function ObtenerDelimitadoresDefinidos(cadenaDeNumeros){
+    if(cadenaDeNumeros.includes("//["))
+    {
+        var definicionDeDelimitadores=cadenaDeNumeros.slice(cadenaDeNumeros.indexOf("[")+1,cadenaDeNumeros.lastIndexOf("] "));
+        let delimitadores=definicionDeDelimitadores.split("][");
+        return delimitadores;
+    } 
+    return [];
+}
+function QuitarDefinicionDeDelimitadores(cadenaDeNumeros){
+    cadenaDeNumeros=cadenaDeNumeros.slice(cadenaDeNumeros.indexOf("] ")+2);
+    return cadenaDeNumeros;
+
+}
+function cambiarDelimitadores(cadenaDeNumeros,listaDeDelimitadores){
+    
+    for(var i=0;i<listaDeDelimitadores.length;i++)
+    {
+        cadenaDeNumeros=cadenaDeNumeros.replaceAll(listaDeDelimitadores[i],",");
+    }
+    return cadenaDeNumeros;
+}
+function sumarListaDeNumeros(ListaDeNumeros){
     let total_suma=0;
     for(var i=0;i<ListaDeNumeros.length;i++)
     {
